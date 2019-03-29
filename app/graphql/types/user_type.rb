0b4738c0d42baf14855e23ed8b630287b8798dbc
occmee <occmee@gmail.com>
2019-03-29 14:55:30 +0900

    Add field

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,5 +4,7 @@ module Types
     field :first_name, String, null: false
     field :last_name, String, null: false
     field :email, String, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
